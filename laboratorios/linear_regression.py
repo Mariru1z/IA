ee62a73b0a905510fe443613981e73b0ca87ab14
Mariru1z <maria.ruizsa@anahuac.mx>
2024-04-25 11:44:30 -0600

    Agregado archivo nombre_del_archivo

--- laboratorios/linear_regression.py
+++ laboratorios/linear_regression.py
@@ -3,4 +3,21 @@ from sklearn.linear_model import LinearRegression #import the linear regression 
 import matplotlib.pyplot as plt # the plotting linbrary
 import numpy as np
 
-x=np.array() 
+x = np.array([121, 125, 131, 141, 152, 161]).reshape(-1,1) # x denotes the house area as a feature. 
+y = np.array([300, 350, 425, 405,496,517]) # y denotes the house price.
+plt.scatter(x,y)
+plt.xlabel("area") # X axis indicates the area.
+plt.ylabel("price") # Y axis indicates the price.
+plt.show()
+lr = LinearRegression() # Encapsulate the linear regression model into an object. 
+lr.fit(x,y) # Train the model on the dataset.
+w = lr.coef_# Slope of the model
+print('Slope: ',w)
+print('Intercept: ',b)
+plt.scatter(x,y)
+plt.xlabel("area") # X axis indicates the area. 
+plt.ylabel("price") # Y axis indicates the price.
+plt.plot([x[0],x[-1]],[x[0]*w+b,x[-1]*w+b])
+
+testX = np.array([[130]])# A test sample with an area of 130
+lr.predict(testX)
